9e014098b80037856d64530fa065eec332c5cbeb
ecom-square <101410924+ecom-square@users.noreply.github.com>
2022-03-24 18:47:05 +0600

    if, else if , else practice

---
+++ js/condition.js
@@ -0,0 +1,82 @@
+// Between Two Number
+// let a=20;
+// let b=20;
+
+// if(a>b){
+//     console.log("True")
+// } 
+// if(a<b){
+//     console.log("False")
+// }
+// if(a==b){
+//     console.log(" Both are equal")
+// }
+
+
+// Between Three Number
+
+
+// let a=10;
+// let b=10;
+// let c=20;
+
+
+// if(a>b && a>c){
+//     console.log("A is Big")
+// } 
+// if(a<b && a<c){
+//     console.log("A is Small")
+// }
+// if(b>c){
+//     console.log("B is Big than C")
+// }
+// if(b<c){
+//     console.log("B is Small than C")
+// } 
+// if(a==b){
+//     console.log("A and B are equal")
+// }
+// if(b==c){
+//     console.log("B and C are equal")
+// }
+// if(c==a){
+//     console.log("C and A is equal")
+// }
+
+// If statement fo Positive, Negative & Zero
+
+// let x=10;
+
+// if(x>0){
+//     console.log("Positive")
+// } 
+
+// if(x<0){
+//     console.log("Negative")
+// } 
+// if(x==0){
+//     console.log("Zero")
+// }
+
+
+
+// Hot, Cold & Normal
+
+// let temp=20;
+
+// if(temp>=30){
+// console.log("Hot")
+// } else if(temp<=20){
+// console.log("Cold")
+// } else{console.log("Normal")}
+
+
+// Even Number & Odd Number
+
+// let givenNumber=1000;
+
+// if(givenNumber%2==0){
+// console.log("Even Number")
+// } else{console.log("Odd Number")}
+
+
